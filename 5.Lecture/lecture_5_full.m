ee5cf34e07bc42bdcb21fb0c2340071da0bb0635
Steigner <200543@seznam.cz>
2022-10-21 08:15:42 +0200

    spaces

--- 5.Lecture/lecture_5_full.m
+++ 5.Lecture/lecture_5_full.m
@@ -40,7 +40,7 @@ figure(1)
 hold on
 
 for n=1:N
-    plot(n,fakt(n),'bo')
+    plot(n, fakt(n), 'bo')
 end
 
 % Program vykresli hodnoty pole F do x-y grafu, 
@@ -83,8 +83,8 @@ hold on
 
 x=1:2:N-1;
 for n=x
-    plot(n,log(fakt(n)),'ko')
-    plot(n+1,log(fakt(n+1)),'k*')
+    plot(n, log(fakt(n)), 'ko')
+    plot(n+1, log(fakt(n+1)), 'k*')
 end
 
 %% PLOT
@@ -95,9 +95,9 @@ x = (0:pi/20:2*pi);
 y1 = sin(x);
 y2 = cos(x);
 
-plot(x,y1,'b--',x,y2,'r--');
+plot(x, y1, 'b--', x, y2, 'r--');
 grid on
-legend('sin(x)','cos(x)');
+legend('sin(x)', 'cos(x)');
 xlabel('x');
 ylabel('y');
 
@@ -106,10 +106,10 @@ ylabel('y');
 % stupnice o zakladu 10 na ose y.
 
 figure(1)
-plot(1:20,exp(1:20));
+plot(1:20, exp(1:20));
 
 figure(2)
-semilogy(1:20,exp(1:20));
+semilogy(1:20, exp(1:20));
 
 %% Polar
 % vykresli primku v polarnich souradnicich, pricemz theta(prvni vstupni parametr) 
@@ -122,7 +122,7 @@ n = length(t);
 % R = ones(1,n);
 R = 0:1/n:1-1/n;
 
-polar(t,R,'bo')
+polar(t, R, 'bo')
 
 %% Bar
 % Vytvori sloupcovy graf s jednim sloupcem pro kazdy prvek y
@@ -134,12 +134,12 @@ bar([1 2; 2 3; 3 4])
 % Vykresli kolacovy graf s pouzitim dat ve vstupnim vektoru x.
 
 explode = [0 1 0 1 0 1];
-pie([1 2 3 4 5 6],explode);
+pie([1 2 3 4 5 6], explode);
 
 %% Hist
 % Vypocita a zobrazi histogram datové rady.
 
-x = rand(1,10000);
+x = rand(1, 10000);
 hist(x)
 
 %% Sroubovice - Plot3D, subplot
@@ -147,7 +147,7 @@ hist(x)
 clear;
 clc;
 
-t = linspace(0,10*pi,1e3);
+t = linspace(0, 10*pi, 1e3);
 r = 2;
 b = 1/(2 * pi);
 z = b * t;
@@ -163,11 +163,11 @@ title('Sroubovice')
 % subplot(1,2,1), subplot(1,2,2), subplot(1,2,3) >> Error
 
 subplot(1,2,1)
-plot3(x,y,z)
+plot3(x, y, z)
 grid on
 
 subplot(1,2,2)
-plot3(x.*z,y.*z,z)
+plot3(x.*z, y.*z, z)
 grid on
 
 %% ! Meshgrid !
@@ -178,15 +178,15 @@ grid on
 % Vystupni hodnoty z meshgrid [X,Y] kde X je matice, kdy kazdy radek je kopii
 % vstupniho vektoru x, a Y je matice, kde kazdy sloupec je kopii vektoru y. Mrizka 
 % reprezentovana souradnicemi X a Y ma radky length(y) a sloupce length(x).
-[X,Y] = meshgrid(1:5,5:10)
+[X,Y] = meshgrid(1:5, 5:10)
 % vystupem tedy je matice X ktery ma 1 az 5 ve sloupci a ma 6 radku
 % vystupem tedy je matice Y ktery ma 5 az 10 v radich a ma 6 sloupcu
 
 %% Sombrero mesh, surf
 
-dx = linspace(-8,8,50);
+dx = linspace(-8, 8, 50);
 dy = dx;
-[x,y] = meshgrid(dx,dy);
+[x,y] = meshgrid(dx, dy);
 
 r = sqrt(x.^2 + y.^2);
 z = sin(r)./r;
@@ -195,11 +195,11 @@ figure(1)
 subplot(1,2,1)
 % Vytvori sitovou plochu, coz je trojrozmerny povrch, ktery ma plne barvy okraju 
 % a zadne barvy ploch.
-mesh(x,y,z)
+mesh(x, y, z)
 
 subplot(1,2,2)
 % Vytvori trojrozmerny povrch, ktery ma plne barvy okraju 
 % a plne barvy ploch.
-surf(x,y,z)
+surf(x, y, z)
 
 
