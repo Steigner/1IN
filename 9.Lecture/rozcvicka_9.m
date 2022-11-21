e2fe9e200caa2ccb470c4ad9c01edb12f6a026c9
Steigner <200543@seznam.cz>
2022-11-21 20:15:00 +0100

    Create rozcvicka_9.m

---
+++ 9.Lecture/rozcvicka_9.m
@@ -0,0 +1,14 @@
+% Vytvorte funkci, ktera secte dve cisla a vrati vysledek
+
+% Vytvorte funkci, ktera vynasobi dve cisla a vrati vysledek
+
+% vytvorte funkci, ktera vyuzije predchozi funkce fSum a fProd, a dale posle na standardni 
+% vystup textovy retezec se zadanim a vysledky danych operaci (viz program), k tomuto bodu 
+% poznamenejme, ze funkce prozatim nic nevraci
+
+% Modifikujte funkci:
+% a) pouze jeden fprintf
+
+% b) funkce navic vraci textovy retezec ekvivalentni s predchozim vystupem funkce fprintf
+
+% c) funkce ulozi uvedeny textovy retezec do souboru data.txt
